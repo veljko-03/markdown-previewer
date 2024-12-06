e326ac9418bbfeb185a8448fa9f613a305e5a797
TheVSDev <vspasic03@gmail.com>
2024-12-06 20:17:34 +0100

    chore: design

--- src/components/defaultMarkdown.js
+++ src/components/defaultMarkdown.js
@@ -1,21 +1,31 @@
 const defaultMarkdown = `# Welcome to Markdown Previewer
-## Created by: Veljko SPASIC
+## Created by: Veljko SPASIC>
+<br />
+
 [Veljko Spasic](https://www.veljko.app)
+<br />
 
 \`Inline code\`
+<br />
 
 \`\`\`
 Block of code
 console.log("Hello, world!");
+
 \`\`\`
+<br />
 
 - List item 1
 - List item 2
 - List item 3
 
+<br />
+
 > Blockquote example
 
 ![Markdown Logo](https://markdown-here.com/img/icon256.png)
+
+<br />
 
 **Bold Text**
 `;
